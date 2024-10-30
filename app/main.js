8a4d264ea998c33189cf859d88f2cf119b677793
Matias Garcia <matias.garcia@tiendanube.com>
2024-10-30 20:57:10 -0300

    respond to multiple pings

--- app/main.js
+++ app/main.js
@@ -3,7 +3,10 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
  const server = net.createServer((connection) => {
-  connection.write('+PONG\r\n')
+  connection.on('data', (stream) => {
+    console.debug('received', stream.toString());
+    connection.write('+PONG\r\n');
+  })
  });
 
  server.listen(6379, "127.0.0.1");
