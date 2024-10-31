249583df72304af51b3b504f381ccf4cb9d207bb
Matias Garcia <matias.garcia@tiendanube.com>
2024-10-30 21:34:53 -0300

    implement the ECHO command

--- app/main.js
+++ app/main.js
@@ -2,10 +2,27 @@ const net = require("net");
 
 console.log("Logs from your program will appear here!");
 
+// https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
  const server = net.createServer((connection) => {
   connection.on('data', (stream) => {
-    console.debug('received', stream.toString());
-    connection.write('+PONG\r\n');
+    const tokens = stream.toString().split('\r\n');
+    console.debug(tokens);
+    if(tokens.length === 0) {
+      return;
+    }
+    const command = tokens[2];
+    switch(command) {
+      case 'PING':
+        connection.write(`+PONG\r\n`);
+        break;
+      case 'ECHO':
+        const arg1 = tokens[4];
+        connection.write(`$${arg1.length}\r\n${arg1}\r\n`);
+        break;
+      default:
+        console.error('unknown command', command);
+      }
+    connection.end();
   })
  });
 
