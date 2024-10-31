38722a9898267351d627c3516eecb3d37ef364e0
Matias Garcia <matias.garcia@tiendanube.com>
2024-10-30 21:51:41 -0300

    implement SET & GET

--- app/main.js
+++ app/main.js
@@ -2,28 +2,50 @@ const net = require("net");
 
 console.log("Logs from your program will appear here!");
 
+function encodeValue(value) {
+  if(value === undefined || value === null) {
+    return `$-1\r\n`
+  }
+  return `$${value.length}\r\n${value}\r\n`;
+}
+
+const map = {};
 // https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
- const server = net.createServer((connection) => {
+const server = net.createServer((connection) => {
   connection.on('data', (stream) => {
     const tokens = stream.toString().split('\r\n');
     console.debug(tokens);
-    if(tokens.length === 0) {
+    if (tokens.length === 0) {
       return;
     }
     const command = tokens[2];
-    switch(command) {
-      case 'PING':
+    switch (command) {
+      case 'PING': 
         connection.write(`+PONG\r\n`);
         break;
-      case 'ECHO':
+      case 'ECHO': {
         const arg1 = tokens[4];
-        connection.write(`$${arg1.length}\r\n${arg1}\r\n`);
+        connection.write(encodeValue(arg1));
         break;
+      }
+      case 'SET': {
+        const key = tokens[4];
+        const value = tokens[6];
+        map[key] = value;
+        connection.write(`+OK\r\n`);
+        break;
+      }
+      case 'GET': {
+        const key = tokens[4];
+        const value = map[key];
+        connection.write(encodeValue(value))
+        break;
+      }
       default:
         console.error('unknown command', command);
         break;
-      }
+    }
   })
- });
+});
 
- server.listen(6379, "127.0.0.1");
+server.listen(6379, "127.0.0.1");
