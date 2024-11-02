fec1707d0c5babe17f8d0807fb02d6e78be85c2a
Matias Hernan Garcia <matias@MBP-de-Matias-2.fibertel.com.ar>
2024-11-01 23:10:39 -0300

    CLI tests (cli-test-1730513439545)

--- your_program.sh
+++ your_program.sh
@@ -12,4 +12,6 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
+npm install
+npm run build
 exec node app/main.js "$@"
