31022ab571bba5becbd4e858b2ad1039685f2049
Matias Hernan Garcia <matias@MBP-de-Matias-2.fibertel.com.ar>
2024-11-10 18:47:57 -0300

    send handshake pt3: implement psync

--- app/main.ts
+++ app/main.ts
@@ -95,6 +95,10 @@ function receiveCommands(connection: net.Socket) {
         }
         break;
       }
+      case 'PSYNC': {
+        write(connection, Encoder.encodeValue(new SimpleString(`FULLRESYNC ${config.masterReplid} 0`)));
+        break;
+      }
       default:
         console.error('unknown command', command);
         break;
