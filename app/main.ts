050e4cb79bac01af525b78dd6f9a5b1baf3a1e73
Matias Garcia <matias.garcia@tiendanube.com>
2024-11-11 10:18:18 -0300

    CLI tests (cli-test-1731331098279)

--- app/main.ts
+++ app/main.ts
@@ -7,7 +7,7 @@ import * as Rdb from './rdb.js';
 import { BulkString } from './bulkString.js';
 import { SimpleString } from './simpleString.js';
 import { performHandshake } from './replica/handshake.js';
-import { EMPTY_RDB_FILE } from './const.js';
+import { CRLF_TERMINATOR, EMPTY_RDB_FILE } from './const.js';
 
 const config = Config.loadConfiguration(process.argv.slice(2, process.argv.length))
 const rdb = Rdb.readRdbFile(config.rdbFileDir, config.dbFileName);
@@ -26,94 +26,121 @@ function forwardWrite(val: Buffer | string) {
   replicaConnections.forEach((replicaConnection) => write(replicaConnection, val))
 }
 
+function collectCommands(stream: Buffer) {
+  return scanCommands(stream).map(c => c.join(CRLF_TERMINATOR));
+}
+
+function scanCommands(stream: Buffer) {
+  const chunks = stream.toString().split(CRLF_TERMINATOR);
+  const commands: Array<Array<string>> = [];
+  for(let i = 0; i < chunks.length;) {
+    const element = chunks[i];
+    if(!element) { return commands }
+    if(element[0] !== '*') throw new Error('unexpected');
+    const length = Number(element.substring(1));
+    const amountOfElementsToCollect = length * 2; // each element has its two first bytes that indicate the type of value
+    const endIndex = i + amountOfElementsToCollect + 1
+    const elementsToCollect = chunks.slice(i, endIndex);
+    commands.push(elementsToCollect);
+    i = endIndex;
+  }
+
+  return commands;
+}
+
+function processCommand(stream: string, connection: net.Socket) {
+  const tokens = stream.split(CRLF_TERMINATOR);
+  console.debug(`>> ${JSON.stringify(stream.toString())}`);
+  if (tokens.length === 0) {
+    return;
+  }
+  const command = tokens[2]?.toUpperCase() ?? '';
+  if(command === 'SET') forwardWrite(stream);
+  switch (command) {
+    case 'PING':
+      write(connection, Encoder.encodeValue(new SimpleString('PONG')));
+      break;
+    case 'ECHO': {
+      const arg1 = tokens[4];
+      write(connection, Encoder.encodeValue(arg1));
+      break;
+    }
+    case 'SET': {
+      const key = tokens[4];
+      const value = tokens[6];
+      const opts = tokens[8] ?? '';
+      const expirationTime = tokens[10];
+      const options = opts.toUpperCase() === "PX" ? { expiresInMilliseconds: Number(expirationTime) } : {};
+      Database.set(key, value, options)
+      write(connection, Encoder.encodeValue(new SimpleString('OK')));
+      break;
+    }
+    case 'GET': {
+      const key = tokens[4];
+      const value = Database.get(key);
+      write(connection, Encoder.encodeValue(value))
+      break;
+    }
+    case 'CONFIG': {
+      const command = Utils.safeUppercase(tokens[4]);
+      if(command !== "GET") return;
+      const valueToGet = tokens[6];
+      if(valueToGet === 'dir') {
+        write(connection, Encoder.encodeValue(['dir', Config.get().rdbFileDir]));
+      } else if(valueToGet === 'dbfilename') {
+        write(connection, Encoder.encodeValue(['dbfilename', Config.get().dbFileName]));
+      }
+      break;
+    }
+    case 'KEYS': {
+      const key = tokens[4];
+      if (key == "*") {
+        write(connection, Encoder.encodeValue(Database.keys()))
+      }
+      break;
+    }
+    case 'INFO': {
+      const key = tokens[4];
+      if(key === undefined) {
+        const configInfo = Config.infoConfigKeys().map(k => `${Utils.camelToSnakeCase(k)}:${config[k]}`)
+        write(connection, Encoder.encodeValue(new BulkString(configInfo)));
+      } else if(key === "replication") {
+        const info = Object.entries(Config.getReplicationInfo()).map(([k, v]: [k: string, v: unknown]) => 
+          `${Utils.camelToSnakeCase(k)}:${v}`
+        )
+        write(connection, Encoder.encodeValue(new BulkString(info)));
+      } else if(Config.isInfoConfigKey(key)) {
+        write(connection, Encoder.encodeValue(`${Utils.camelToSnakeCase(key)}:${config[key]}`));
+      } else {
+        throw new Error(`invalid argument: ${key}`)
+      }
+      break;
+    }
+    case 'REPLCONF': {
+      const key = tokens[4];
+      const value = tokens[6]; // do nothing for now
+      if(key === 'listening-port' || key === 'capa') {
+        write(connection, Encoder.encodeValue(new SimpleString('OK')));
+      }
+      break;
+    }
+    case 'PSYNC': {
+      write(connection, Encoder.encodeValue(new SimpleString(`FULLRESYNC ${config.masterReplid} ${config.masterReplOffset}`)));
+      write(connection, Encoder.encodeValue(EMPTY_RDB_FILE));
+      // Handshake finished, assume RDB File was processed correctly
+      replicaConnections.push(connection);
+      break;
+    }
+    default:
+      console.error('unknown command', command);
+      break;
+  }
+}
+
 function receiveCommands(connection: net.Socket) {
   connection.on('data', (stream) => {
-    const tokens = stream.toString().split('\r\n');
-    console.debug(`>> ${JSON.stringify(stream.toString())}`);
-    if (tokens.length === 0) {
-      return;
-    }
-    const command = tokens[2]?.toUpperCase() ?? '';
-    if(command === 'SET') forwardWrite(stream);
-    switch (command) {
-      case 'PING':
-        write(connection, Encoder.encodeValue(new SimpleString('PONG')));
-        break;
-      case 'ECHO': {
-        const arg1 = tokens[4];
-        write(connection, Encoder.encodeValue(arg1));
-        break;
-      }
-      case 'SET': {
-        const key = tokens[4];
-        const value = tokens[6];
-        const opts = tokens[8] ?? '';
-        const expirationTime = tokens[10];
-        const options = opts.toUpperCase() === "PX" ? { expiresInMilliseconds: Number(expirationTime) } : {};
-        Database.set(key, value, options)
-        write(connection, Encoder.encodeValue(new SimpleString('OK')));
-        break;
-      }
-      case 'GET': {
-        const key = tokens[4];
-        const value = Database.get(key);
-        write(connection, Encoder.encodeValue(value))
-        break;
-      }
-      case 'CONFIG': {
-        const command = Utils.safeUppercase(tokens[4]);
-        if(command !== "GET") return;
-        const valueToGet = tokens[6];
-        if(valueToGet === 'dir') {
-          write(connection, Encoder.encodeValue(['dir', Config.get().rdbFileDir]));
-        } else if(valueToGet === 'dbfilename') {
-          write(connection, Encoder.encodeValue(['dbfilename', Config.get().dbFileName]));
-        }
-        break;
-      }
-      case 'KEYS': {
-        const key = tokens[4];
-        if (key == "*") {
-          write(connection, Encoder.encodeValue(Database.keys()))
-        }
-        break;
-      }
-      case 'INFO': {
-        const key = tokens[4];
-        if(key === undefined) {
-          const configInfo = Config.infoConfigKeys().map(k => `${Utils.camelToSnakeCase(k)}:${config[k]}`)
-          write(connection, Encoder.encodeValue(new BulkString(configInfo)));
-        } else if(key === "replication") {
-          const info = Object.entries(Config.getReplicationInfo()).map(([k, v]: [k: string, v: unknown]) => 
-            `${Utils.camelToSnakeCase(k)}:${v}`
-          )
-          write(connection, Encoder.encodeValue(new BulkString(info)));
-        } else if(Config.isInfoConfigKey(key)) {
-          write(connection, Encoder.encodeValue(`${Utils.camelToSnakeCase(key)}:${config[key]}`));
-        } else {
-          throw new Error(`invalid argument: ${key}`)
-        }
-        break;
-      }
-      case 'REPLCONF': {
-        const key = tokens[4];
-        const value = tokens[6]; // do nothing for now
-        if(key === 'listening-port' || key === 'capa') {
-          write(connection, Encoder.encodeValue(new SimpleString('OK')));
-        }
-        break;
-      }
-      case 'PSYNC': {
-        write(connection, Encoder.encodeValue(new SimpleString(`FULLRESYNC ${config.masterReplid} ${config.masterReplOffset}`)));
-        write(connection, Encoder.encodeValue(EMPTY_RDB_FILE));
-        // Handshake finished, assume RDB File was processed correctly
-        replicaConnections.push(connection);
-        break;
-      }
-      default:
-        console.error('unknown command', command);
-        break;
-    }
+    const commands = collectCommands(stream);
+    commands.forEach(c => processCommand(c, connection))
   })
 }
 
