3d1557a57048b8741e584988bfb2620722081ff0
Matias Hernan Garcia <matias@MacBook-Pro-de-Matias-2.local>
2024-11-16 11:29:09 -0300

    test: add tests for collectCommands

---
+++ app/collectCommands.test.ts
@@ -0,0 +1,13 @@
+import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
+import { collectCommands } from './collectCommands';
+
+describe('#collectCommands', () => {
+  it('should split stream of two commands', () => {
+    const result = collectCommands(
+      Buffer.from("*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n123\r\n*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n")
+    )
+    assert.equal(result[0], '*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n123\r\n');
+    assert.equal(result[1], '*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n');
+  });
+});
