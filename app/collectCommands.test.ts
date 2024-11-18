933e24f37c9d46f1aa52b33ccc742de196bdbb94
matiasgarcia <garcia.matias92@gmail.com>
2024-11-18 08:30:04 -0300

    wip

--- app/collectCommands.test.ts
+++ app/collectCommands.test.ts
@@ -7,7 +7,30 @@ describe('#collectCommands', () => {
     const result = collectCommands(
       Buffer.from("*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n123\r\n*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n")
     )
-    assert.equal(result[0], '*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n123\r\n');
-    assert.equal(result[1], '*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n');
+    assert.deepEqual(result.length, 2);
+    assert.deepEqual(result[0], Buffer.from('*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n123\r\n'));
+    assert.deepEqual(result[1], Buffer.from('*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n'));
   });
+
+  it('should parse single command', () => {
+    const expected = Buffer.from("$3\r\nSET\r\n");
+    const result = collectCommands(
+      expected
+    )
+    assert.deepEqual(result[0], expected);
+  })
+
+  it('should split buffered commands into two', () => {
+    const result = collectCommands(Buffer.from("+FULLRESYNC 75cd7bc10c49047e0d163660f3b90625b1af31dc 0\r\n$88\r\nREDIS0011\xfa\tredis-ver\x057.2.0\xfa\nredis-bits\xc0@\xfa\x05ctime\xc2m\b\xbce\xfa\bused-mem°\xc4\x10\x00\xfa\baof-base\xc0\x00\xff\xf0n;\xfe\xc0\xffZ\xa2"));
+    assert.equal(result.length, 2);
+    assert.deepEqual(result[0], Buffer.from("+FULLRESYNC 75cd7bc10c49047e0d163660f3b90625b1af31dc 0\r\n"));
+    assert.deepEqual(result[1], Buffer.from("$88\r\nREDIS0011\xfa\tredis-ver\x057.2.0\xfa\nredis-bits\xc0@\xfa\x05ctime\xc2m\b\xbce\xfa\bused-mem°\xc4\x10\x00\xfa\baof-base\xc0\x00\xff\xf0n;\xfe\xc0\xffZ\xa2"));
+  })
+
+  it('parses RESP arrays', () => {
+    const result = collectCommands(Buffer.from("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"));
+    assert.equal(result.length, 2);
+    assert.deepEqual(result[0], Buffer.from("$5\r\nhello\r\n"));
+    assert.deepEqual(result[1], Buffer.from("$5\r\nworld\r\n"));
+  })
 });
--- app/collectCommands.ts
+++ app/collectCommands.ts
@@ -1,18 +1,78 @@
 import { CRLF_TERMINATOR, EMPTY_RDB_FILE } from './const.js';
+import { invariant } from './utils.js';
 
 export function collectCommands(stream: Buffer) {
   const firstByte = stream.at(0);
   if(!firstByte) throw new Error('no firstByte');
   const firstByteIdentifier = String.fromCharCode(firstByte);
-  switch(firstByteIdentifier) {
-    case '+':
-    case '$':
-      return [stream.toString()];
-    case '*':
-      return scanCommands(stream);
-    default:
-      throw new Error(`Unexpected format: ${firstByte} ${firstByteIdentifier}`);
-  }
+  return scanCommands2(stream);
+}
+
+export function scanCommands2(stream: Buffer): Array<Buffer> {
+  let i = 0;
+  let commands: Array<Buffer> = [];
+
+  do {
+    const byte = stream.at(i);
+    invariant(typeof byte !== 'undefined', 'expected byte');
+    switch(String.fromCharCode(byte)) {
+      case '+': {
+        const startOfCLRF = stream.indexOf(CRLF_TERMINATOR, i);
+        if(startOfCLRF !== -1) {
+          const endOfCommandIndex = startOfCLRF + 1;
+          commands.push(stream.subarray(i, endOfCommandIndex + 1));
+          i = endOfCommandIndex + 1;
+        } else {
+          throw new Error('Unknown...');
+        }
+        break;
+      }
+      case '$': {
+        const startOfLength = stream.indexOf(CRLF_TERMINATOR, i);
+        if(startOfLength === -1) throw new Error('Missing endOfLength for $');
+
+        // Parse the length of the value
+        const encodedValueLength = stream.subarray(i + 1, startOfLength).toString();
+        const valueLength = Number(encodedValueLength);
+        if (Number.isNaN(valueLength)) throw new Error('Value length is not a number.');
+
+        // Calculate the end of the value
+        const valueStartIndex = startOfLength + 1;
+        const endIndex = stream.indexOf(CRLF_TERMINATOR, valueStartIndex);
+        if (endIndex === -1) {
+          // binary
+          const command = stream.subarray(i);
+          commands.push(command);
+          i += command.length + 1;
+        } else {
+          const command = stream.subarray(i, endIndex + 2)
+          commands.push(command);
+          i += command.length;
+        }
+        break;
+      }
+      case '*': {
+        const startOfLength = stream.indexOf(CRLF_TERMINATOR, i);
+        if (startOfLength === -1) throw new Error('Missing end of length for * command.');
+        const endOfLengthIndex = startOfLength + CRLF_TERMINATOR.length;
+        const encodedArrayLength = stream.subarray(i + 1, startOfLength).toString();
+        const arrayLength = Number(encodedArrayLength);
+        if (Number.isNaN(arrayLength)) throw new Error('Array length is not a number.');
+
+        const arrayCommands = scanCommands2(stream.subarray(endOfLengthIndex));
+
+        commands = commands.concat(arrayCommands);
+        const commandsByteSize = arrayCommands.reduce((acc, curr) => acc + curr.byteLength, 0);
+        const commandByteSize = 1 + encodedArrayLength.length + CRLF_TERMINATOR.length + commandsByteSize;
+        i += commandByteSize
+        break;
+      }
+      default:
+        throw new Error(`Unexpected byte ${byte?.toString()} ${String.fromCharCode(byte)}`)
+    }
+  } while(i < stream.length);
+
+  return commands;
 }
 
 function scanCommands(stream: Buffer): Array<string> {
